8db0549774813aada7f74f23c69e50753566672c
marknascimento <mark_nascimento@zoho.com>
2017-06-06 01:09:03 -0400

    Final update: 95% done
    Fixed a couple more mistakes and typos, including making the array references properly zero based, accounting for the 9 structure, and making the object oriented reference properly at the end. Still not functional, but a lot closer now.

--- RomanC.rb
+++ RomanC.rb
@@ -1,36 +1,45 @@
 class RomanNumeralCalculator
   def calculateDigit (myAmount, myPlace)
     if myPlace.to_i == 1
-      myLetters = ["I","V"]
+      myLetters = ["I","V","X"]
     elsif myPlace.to_i == 10
-      myLetters = ["X","L"]
+      myLetters = ["X","L","C"]
     elsif myPlace.to_i == 100
-      myLetters = ["C","D"]
+      myLetters = ["C","D","M"]
     elsif myPlace.to_i == 1000
-      myLetters = ["M",""]
+      myLetters = ["M","",""]
     end
-    if myAmount.to_i < 4
-      myAmount.to_i.times {myDResult += myLetters[1]}
-    elsif myAmount.to_i == 4
-      myDResult = myLetters[1] + myLetters[2]
-    elsif myAmount.to_i == 5
-      myDResult += myLetters[2]
-    elsif myAmount.to_i > 5
-      myDResult = "" + myLetters[2]
+    if myAmount.to_i == 9
+      myDResult =  myLetters[0] + myLetters[2]	  
+    elsif myAmount.to_i > 6
+      myDResult = "" + myLetters[1]
       myAmount.to_i -= 5
-      myAmount.to_i.times {myDResult += myLetters[1]}
+      myAmount.to_i.times {myDResult += myLetters[0]}
+    elsif myAmount.to_i == 5
+      myDResult = myLetters[1]
+    elsif myAmount.to_i == 4
+      myDResult = myLetters[0] + myLetters[1]
+    elsif myAmount.to_i >= 1
+     myAmount.to_i.times {myDResult += myLetters[0]}
+    elsif myAmount.to_i == 0
+      myDResult = ""
     end
+	return myDResult
   end
-  def caluclateNumber (myNumber)
-    myNumber.to_s!
+  def calculateNumber (myNumber)
+    myNumber = myNumber.to_s
     myPlaceNumber = 10**(myNumber.length-1)
-    myNumber.each_char do |c|
-      myNResult += calculateDigit (c, myPlaceNumber)
+    myNumber.each_char do |ch|
+      myNResult += calculateDigit (ch, myPlaceNumber)
       myPlaceNumber = myPlaceNumber.to_i / 10
     end
+	return myNResult
   end
 end
 
 print "Enter a number: "
 convertThis = gets.chomp!
-puts RomanNumeralCalculator(convertThis)
+myCalc = RomanNumeralCalculator.new()
+puts myCalc.calculateNumber(convertThis)
+puts "\n\nAnd now we pause so that you can enjoy looking at the results.\nPress Enter to exit the program."
+allDone = gets.chomp!
